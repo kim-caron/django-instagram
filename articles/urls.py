0f8329a5da95a6db8b383e96d973633ee2eab13d
tjdvjd1160 <tjdvjd1160@gmail.com>
2024-04-25 22:46:12 +0900

    Feat: article app 모델 설계, url 경로 정의, view 함수 메서드 정리

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
+from . import views
 
 app_name = 'articles'
 
 urlpatterns = [
-
+    path('create/', views.create, name='create'),
+    path('<str:article_key>/', views.detail, name='detail'),
+    path('<str:article_key>/update/', views.update, name='update'),
+    path('<str:article_key>/delete/', views.delete, name='delete'),
+    path('<str:article_key>/likes/', views.likes, name='likes'),
+    path('<str:article_key>/save/', views.save, name='save'),
+    path('<str:article_key>/create_comments/', views.create_comments, name='create_comments'),
+    path('<str:article_key>/<int:comment_pk>/like_comments/', views.like_comments, name='like_comments'),
+    path('<str:article_key>/<int:comment_pk>/delete_comments/', views.delete_comments, name='delete_comments'),
+    path('<str:article_key>/<int:comment_pk>/create_subcomments/', views.create_subcomments, name='create_subcomments'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -3,3 +3,32 @@ from django.shortcuts import render, redirect
 def index(request):
     pass
 
+def create(request):
+    pass
+
+def detail(request, article_key):
+    pass
+
+def update(request, article_key):
+    pass
+
+def delete(request, article_key):
+    pass
+
+def likes(request, article_key):
+    pass
+
+def save(request, article_key):
+    pass
+
+def create_comments(request, article_key):
+    pass
+
+def delete_comments(request, article_key, comment_pk):
+    pass
+
+def like_comments(request, article_key, comment_pk):
+    pass
+
+def create_subcomments(request, article_key, comment_pk):
+    pass
